06a94899f2de096f0ff31cdb6fc7e8db94cf4662
Ted Michael Celis <krazete@gmail.com>
2022-01-10 18:34:19 -0800

    reverted tally structure to change weight easily, changed size parameter to scale

--- gradient_transfer.py
+++ gradient_transfer.py
@@ -1,32 +1,32 @@
 from PIL import Image
 
-def getGradientMap(image, size=None, justcompare=False):
-    gradient = image.convert('RGBA')
-    if size:
-        gradient = gradient.resize(size)
+def getGradientMap(image, scale=1, justcompare=False):
+    gradient = image.resize((scale * image.width, scale * image.height)).convert('RGBA')
     gradienti = gradient.convert('L')
 
     tally = {}
     for (r, g, b, a), i in zip(gradient.getdata(), gradienti.getdata()):
-        if a > 0:
-            tally.setdefault(i, [0, 0, 0, 0])
-            tally[i][0] += r * a
-            tally[i][1] += g * a
-            tally[i][2] += b * a
-            tally[i][3] += a
-
+        tally.setdefault(i, {})
+        tally[i].setdefault((r, g, b), 0)
+        tally[i][(r, g, b)] += a
     sparsemap = {}
     for i in tally:
-        r, g, b, a = tally[i]
-        sparsemap[i] = (int(r / a), int(g / a), int(b / a))
+        t, h, n, w = 0, 0, 0, 0
+        for r, g, b in tally[i]:
+            weight = tally[i][(r, g, b)] # weight = 1 works well too
+            t += r * weight
+            h += g * weight
+            n += b * weight
+            w += weight
+        sparsemap[i] = (int(t / w), int(h / w), int(n / w))
     sparsemap.setdefault(0, (0, 0, 0))
     sparsemap.setdefault(255, (255, 255, 255))
 
     power = 1 + len(sparsemap) / 4 # 1 - 65
-    colormap = []
+    gradientmap = []
     for i in range(256):
         if i in sparsemap:
-            colormap.append(sparsemap[i])
+            gradientmap.append(sparsemap[i])
         else:
             t, h, n, w = 0, 0, 0, 0
             for j in sparsemap:
@@ -36,29 +36,27 @@ def getGradientMap(image, size=None, justcompare=False):
                 h += g * weight
                 n += b * weight
                 w += weight
-            colormap.append((int(t / w), int(h / w), int(n / w)))
+            gradientmap.append((int(t / w), int(h / w), int(n / w)))
 
     if justcompare:
         swatch = Image.new('RGB', (256, 2), (0, 255, 0))
-        swatch.putdata(colormap)
+        swatch.putdata(gradientmap)
         for i in sparsemap:
             swatch.putpixel((i, 1), (*sparsemap[i], 255))
         return swatch.resize((256, 256), Image.NEAREST)
 
-    return colormap
+    return gradientmap
 
-def applyGradientMap(image, colormap, size=None):
-    img = image.convert('RGBA')
-    if size:
-        img = img.resize(size)
+def applyGradientMap(image, gradientmap, scale=1):
+    img = image.resize((scale * image.width, scale * image.height)).convert('RGBA')
     imgi = img.convert('L')
 
-    img.putdata([(*colormap[i], a) for (r, g, b, a), i in zip(img.getdata(), imgi.getdata())])
+    img.putdata([(*gradientmap[i], a) for (r, g, b, a), i in zip(img.getdata(), imgi.getdata())])
     return img
 
 if __name__ == '__main__':
-    pinkmap = getGradientMap(Image.open('./input/GiftIcon_Standard.png'))
-    goldmap = getGradientMap(Image.open('./input/GiftIcon_Gold.png'))
+    pinkmap = getGradientMap(Image.open('./input/GiftIcon_Standard.png'), 2)
+    goldmap = getGradientMap(Image.open('./input/GiftIcon_Gold.png'), 2)
 
     molly = Image.open('./input/molly.png')
     applyGradientMap(molly, pinkmap).save('./output/mollypink.png')
@@ -66,4 +64,4 @@ if __name__ == '__main__':
 
     # applyGradientMap(Image.open('./input/pol.png'), goldmap).save('./output/polgold.png')
 
-    # getGradientMap(Image.open('./input/2x2.png'), (16, 16), justcompare=True) # super sparse test
+    # getGradientMap(Image.open('./input/2x2.png'), 8, justcompare=True) # super sparse test
